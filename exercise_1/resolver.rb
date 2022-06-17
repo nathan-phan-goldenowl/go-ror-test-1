7b6697e0332a03fe8a093962e53131af409e507c
GondaKid <thienphuc16066@gmail.com>
2022-06-17 18:14:51 +0700

    done exercise 1

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,20 @@
-# write code here...
+# ƒ(10, [3, 4, 5, 6, 7]) -> [(4, 6), (3, 7)]
+# hash = {7:0, 6:1, 5:2, 4:3, 3:4}
+
+# ƒ(8, [3, 4, 5, 4, 4]) -> [(3, 5)]
+# hash = { 5:0, 4:1, 3:2, 4:3, 4:4 }
+
+def f(sum, list)
+  half_pair_hash = {}
+  res = []
+
+  list.each_with_index do |element, index|
+    res << [list[half_pair_hash[element]], element] if half_pair_hash[element]
+
+    if element != sum / 2
+      half_pair_hash[sum - element] ||= index
+    end
+  end
+
+  res
+end
--- exercise_1/resolver_test.rb
+++ exercise_1/resolver_test.rb
@@ -1,1 +1,23 @@
-# write code here...
+require_relative './resolver.rb'
+
+describe :extract_pair do
+  context 'when resultable list is given' do
+    # ƒ(10, [3, 4, 5, 6, 7]) -> [(4, 6), (3, 7)]
+    let(:sum) { 10 }
+    let(:list) { [3, 4, 5, 6, 7] }
+
+    it 'returns the expected result' do
+      expect(f(sum, list)).to eq([[4, 6], [3, 7]])
+    end
+  end
+
+  context 'when no resultable list is given' do
+    # ƒ(100, [3, 4, 5, 6, 7]) -> []
+    let(:sum) { 100 }
+    let(:list) { [3, 4, 5, 6, 7] }
+
+    it 'returns the expected result' do
+      expect(f(sum, list)).to eq([])
+    end
+  end
+end
